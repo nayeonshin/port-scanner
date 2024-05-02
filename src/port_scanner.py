b9844b01cf6324acca36cb47be0e1a781ff2e701
Nayeon Shin <dev.nayeon@gmail.com>
2024-05-02 18:48:02 -0400

    fix(tcp_syn_scan): check open host instead of open ports

--- src/port_scanner.py
+++ src/port_scanner.py
@@ -2,13 +2,7 @@ from scapy.all import ICMP, IP, TCP, send, sr1
 import random
 import socket
 import argparse
-import sys 
-
-
-def check_is_alive_host(target_host: str) -> bool:
-    icmp_echo_request = IP(dst=target_host) / ICMP()
-    icmp_echo_reply = sr1(icmp_echo_request, timeout=1, verbose=0)
-    return bool(icmp_echo_reply)
+import sys
 
 
 def tcp_connect_scan(host, port):
@@ -40,14 +34,10 @@ def tcp_connect_scan(host, port):
 
 
 def tcp_syn_scan(target_host: str, ports: list[int]) -> list[tuple[int, str]]:
-    def check_is_open_port(port: int) -> bool:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            try:
-                s.connect((target_host, port))
-            except (socket.error, OSError):
-                return False
-
-        return True
+    def check_is_alive_host(target_host: str) -> bool:
+        icmp_echo_request = IP(dst=target_host) / ICMP()
+        icmp_echo_reply = sr1(icmp_echo_request, timeout=1, verbose=0)
+        return bool(icmp_echo_reply)
 
     def get_service_name(port: int) -> str:
         try:
@@ -59,11 +49,11 @@ def tcp_syn_scan(target_host: str, ports: list[int]) -> list[tuple[int, str]]:
 
     open_ports = []
 
-    for port in ports:
-        is_open_port = check_is_open_port(port)
-        if not is_open_port:
-            continue
+    is_alive_host = check_is_alive_host(target_host)
+    if not is_alive_host:
+        return open_ports
 
+    for port in ports:
         syn_packet = IP(dst=target_host) / TCP(dport=port, flags="S")
         response = sr1(syn_packet, timeout=1, verbose=0)
 
