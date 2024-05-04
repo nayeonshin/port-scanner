c459d37956f31d12c74de07af48b8c0cc058b91d
Sophia Dai <sdai33@smith.edu>
2024-05-04 12:41:09 -0400

    fixed tcp_connect and udp_scan

--- src/port_scanner.py
+++ src/port_scanner.py
@@ -5,32 +5,50 @@ import argparse
 import sys
 
 
-def tcp_connect_scan(host, port):
-    ip_packet = IP(dst = host)
-    tcp_packet = TCP(dport = port, flags="S")
-    final_packet = ip_packet/tcp_packet
-    response = sr1(final_packet, timeout=5)
+def tcp_connect(host: str, ports: list[int])-> [list, dict]:
+    def check_is_alive_host(host: str) -> bool:
+        icmp_echo_request = IP(dst=host) / ICMP()
+        icmp_echo_reply = sr1(icmp_echo_request, timeout=1, verbose=0)
+        return bool(icmp_echo_reply)
+        
+    def tcp_connect_if_open(host, port)-> [bool,str]:
+        ip_packet = IP(dst = host)
+        tcp_packet = TCP(dport = port, flags="S")
+        final_packet = ip_packet/tcp_packet
+        response = sr1(final_packet, timeout=5)
 
-    if response is not None:
-        if response.haslayer(TCP):
-            if response.getlayer(TCP).flags == 0x12:
-                new_tcp_packet = TCP(sport=response.dport, dport=response.sport, flags="R")
-                new_packet = ip_packet/new_tcp_packet
-                send(new_packet, verbose=0)
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                try:
-                    s.connect((host, port))
-                    s.send(b"GET / HTTP/1.1\r\nHost: " + host.encode() + b"\r\n\r\n")
-                    banner = s.recv(1024)
-                    print("Port {port} is open")
-                    s.close()
-                    if banner:
-                        data = banner.decode().strip()
-                        print(data)
-                        return [True, data]
-                except ConnectionRefusedError:
-                    print("error")
-        return [False, 0]
+        if response is not None:
+            if response.haslayer(TCP):
+                if response.getlayer(TCP).flags == 0x12:
+                    new_tcp_packet = TCP(sport=response.dport, dport=response.sport, flags="R")
+                    new_packet = ip_packet/new_tcp_packet
+                    send(new_packet, verbose=0)
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    try:
+                        s.connect((host, port))
+                        s.send(b"GET / HTTP/1.1\r\nHost: " + host.encode() + b"\r\n\r\n")
+                        banner = s.recv(1024)
+                        print("Port {port} is open")
+                        s.close()
+                        if banner:
+                            data = banner.decode().strip()
+                            print(data)
+                            return [True, data]
+                    except ConnectionRefusedError:
+                        print("error")
+            return [False, 0]
+            
+    open_ports = []
+    banners = {}
+    is_alive_host = check_is_alive_host(host)
+    if not is_alive_host:
+        return [open_ports, banners]
+
+    for port in ports:
+        if tcp_connect_if_open(host, port)[0]:
+            open_ports.append(port)
+            banners[port] = (tcp_connect_if_open(host, port)[0])
+    return [open_ports, banners]
 
 
 def tcp_syn_scan(target_host: str, ports: list[int]) -> list[tuple[int, str]]:
@@ -69,34 +87,36 @@ def tcp_syn_scan(target_host: str, ports: list[int]) -> list[tuple[int, str]]:
 
     return open_ports
 
-def udp_scan(host, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        s.connect((host, port))
-        s.send(b"GET / HTTP/1.1\r\nHost: " + host.encode() + b"\r\n\r\n")
-        s.settimeout(3)
-        return True
-    except socket.timeout:
-        return False
-    except ConnectionRefusedError:
-        return False
-    return False
-
-def get_open_ports(target: str, mode: str, order: str, ports: str):
+def udp_scan(target_host: str, ports: list[int])-> list[tuple[int, str]]:
+    def check_is_alive_host(host: str) -> bool:
+        icmp_echo_request = IP(dst=host) / ICMP()
+        icmp_echo_reply = sr1(icmp_echo_request, timeout=1, verbose=0)
+        return bool(icmp_echo_reply)
+        
+    def if_port_open(host: str, port: int)-> bool:
+        udp_packet = sr1(IP(dst=host)/UDP(sport=port, dport=port), timeout=2, verbose=0)
+        if udp_packet == None:
+            return True
+        else:
+            if udp_packet.haslayer(ICMP):
+                print(port, "Closed")
+                return False
+            elif udp_packet.haslayer(UDP):
+                print(port, "Open / filtered")
+                return True
+            else:
+                print(port, "Unknown")
+                print(udp_packet.summary())
+                return False
+                
     open_ports = []
-    banners={}
-    if ports == 'all':
-        ports_range = list(range(1, 65535))
-    elif ports == 'known':
-        ports_range = list(range(1, 1023))
-    for port in ports_range:
-        if mode == 'connect':
-            if tcp_connect_scan(target, port)[0]:
-                open_ports.append(port)
-                banners[port] = (tcp_connect_scan(target, port)[1])
-        if mode == 'udp':
-            if udp_scan(target, port):
-                open_ports.append(port)
+    is_alive_host = check_is_alive_host(host)
+    if not is_alive_host:
+        return open_ports
+        
+    for port in ports:
+        if if_port_open(host, port):
+            open_ports.append(port)
     return open_ports
 
 
@@ -108,9 +128,9 @@ def scan_ports(target_host: str, mode: str, order: str, ports: str) -> list:
     KNOWN_PORT_COUNT = 1024
 
     modes_to_functions = {
-        "connect": None,  # TODO
+        "connect": tcp_connect,
         "syn": tcp_syn_scan,
-        "udp": None  # TODO
+        "udp": udp_scan
     }
     scan = modes_to_functions.get(mode)
 
